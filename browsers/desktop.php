76b38e06de67fe11a1703c66ac8b00acae758c0a
dword1511 <zhangchi866@gmail.com>
2011-07-11 12:16:14 +0800

    Fixing appearance.

--- browsers/desktop.php
+++ browsers/desktop.php
@@ -63,7 +63,8 @@ function desktop_theme_avatar($url, $force_large = false) {
 
 function desktop_theme_css() {
 	$out = theme_css();
-	$out .= "<style type='text/css'>.avatar{display:block; height:50px; width:50px; left:5px; margin:0; overflow:hidden; position:absolute;}</style>";
+	$out .= "<style type='text/css'>.avatar{display:block; height:50px; width:50px; left:5px; margin:0; overflow:hidden; position:absolute;}
+.shift{margin-left:58px;min-height:48px;}</style>";
 	return $out;
 }
 
