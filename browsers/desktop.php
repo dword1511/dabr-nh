1254891cab11c2f6384e19eabfb0a2dd5bb50189
dword1511 <zhangchi866@gmail.com>
2011-07-11 12:50:50 +0800

    Adjusting appearance.

--- browsers/desktop.php
+++ browsers/desktop.php
@@ -3,7 +3,7 @@ function desktop_theme_status_form($text = '', $in_reply_to_id = NULL) {
 	if (user_is_authenticated()) {
 		$output = '<form method="post" action="update">
   <fieldset><legend><img src="'.BASE_URL.'twimg/bird_16_blue.png" width="16" height="16" /> What\'s Happening?</legend>
-  <textarea id="status" name="status" rows="3" style="width:95%; max-width: 400px;">'.$text.'</textarea>
+  <textarea id="status" name="status" rows="3" style="width:95%;max-width:400px;">'.$text.'</textarea>
   <div><input name="in_reply_to_id" value="'.$in_reply_to_id.'" type="hidden" /><input type="submit" value="Tweet" /> <span id="remaining">140</span> 
   <span id="geo" style="display: none;"><input onclick="goGeo()" type="checkbox" id="geoloc" name="location" /> <label for="geoloc" id="lblGeo"></label></span></div>
   </fieldset>
@@ -64,7 +64,7 @@ function desktop_theme_avatar($url, $force_large = false) {
 function desktop_theme_css() {
 	$out = theme_css();
 	$out .= "<style type='text/css'>.avatar{display:block; height:50px; width:50px; left:5px; margin:0; overflow:hidden; position:absolute;}
-.shift{margin-left:58px;min-height:48px;margin-right:100px;}</style>";
+.shift{margin-left:58px;min-height:72px;max-width:700px;width:95%}</style>";
 	return $out;
 }
 
