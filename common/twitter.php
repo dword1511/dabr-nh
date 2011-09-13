fea5609a6fcca6bdb8c6311628db0a9d4785533f
dword1511 <zhangchi866@gmail.com>
2011-09-14 01:05:48 +0800

    Experimental fix for broken search page.

--- common/twitter.php
+++ common/twitter.php
@@ -1387,7 +1387,6 @@ function twitter_standard_timeline($feed, $source) {
 						'id' => $status->from_user_id,
 						'screen_name' => $status->from_user,
 						//'profile_image_url' => theme_get_avatar($status),
-						'profile_image_url' => preg_match_one('#src="([^"]*)"#' , $tweet),
 					),
 					'to' => (object) array(
 						'id' => $status->to_user_id,
