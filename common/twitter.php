f53f736783c5254c46c41843c458f5da22861237
dword1511 <zhangchi866@gmail.com>
2011-10-15 16:35:17 +0800

    Fixing geoloc.

--- common/twitter.php
+++ common/twitter.php
@@ -352,7 +352,7 @@ function twitter_media_page($query)
 						消息（可选）：<br />
 						<textarea name='message' style='width:90%; max-width: 400px;' rows='3' id='message'>" . $status . "</textarea><br>
 						<input type='submit' value='发送'><span id='remaining'>120</span>";
-	$content .= file_get_contents('../geoloc.html');
+	$content .= file_get_contents('geoloc.html');
 	$content .= '</form>';
 	$content .= js_counter("message", "120");
 
