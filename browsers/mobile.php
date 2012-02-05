db7e376fe28492c3dc22bb4869b5fb239510ea15
dword1511 <zhangchi866@gmail.com>
2012-02-05 10:49:22 +0800

    Remove silly refresh from mobile menu.

--- browsers/mobile.php
+++ browsers/mobile.php
@@ -6,6 +6,16 @@ function mobile_theme_menu_bottom() {
 }
 
 function mobile_theme_menu_top() {
-	return theme_menu_both('bottom');
+	$links = array();
+	foreach (menu_visible_items() as $url => $page) {
+		if (!$url) $url = BASE_URL;
+		if (isset($page['accesskey'])) $links[] = "<a href='$url' accesskey='{$page['accesskey']}'>".$page['display']."</a> {$page['accesskey']}";
+		else $links[] = "<a href='$url'>".$page['display']."</a>";
+	}
+	if (user_is_authenticated()) {
+		$user = user_current_username();
+		array_unshift($links, "<b><a href='user/$user'>$user</a></b>");
+	}
+	return "<div class='menu menu-$menu'>".implode(' | ', $links).'</div>';
 }
 ?>
