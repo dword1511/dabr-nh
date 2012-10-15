98d85cae4e9a2eb34a2262dbea23a6b62dc8f964
dword1511 <zhangchi866@gmail.com>
2012-10-15 13:57:15 +0800

    Merged SVN r494, thanks mention from @Orz_C.

--- common/user.php
+++ common/user.php
@@ -31,7 +31,7 @@ function user_oauth() {
 
 		// Fetch the user's screen name with a quick API call
 		unset($_SESSION['oauth_request_token_secret']);
-		$user = twitter_process('https://api.twitter.com/account/verify_credentials.json');
+		$user = twitter_process('https://api.twitter.com/1.1/account/verify_credentials.json');
 		$GLOBALS['user']['username'] = $user->screen_name;
 
 		_user_save_cookie(1);
