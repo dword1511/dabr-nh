862da0e46fe2778cb4bfa01e477dbde50f64759a
dword1511 <zhangchi866@gmail.com>
2013-04-15 11:49:49 +0800

    Fix Cookie issue and add support for HTTP 20x without Content-Length.

--- simpleproxy.php
+++ simpleproxy.php
@@ -37,7 +37,8 @@ if(!$matches) {
 $req         = getallheaders();
 $req['Host'] = $matches[1];
 $request     = "GET $url HTTP/1.1\r\n";
-foreach($req as $key => $value) $request .= "$key: $value\r\n";
+foreach($req as $key => $value)
+  if($key != 'Cookie') $request .= "$key: $value\r\n"; // Do not leak clients' cookie.
 $request    .= "\r\n";
 $ip          = gethostbyname($req['Host']);
 $socket      = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
@@ -64,14 +65,24 @@ while(strpos($res, "\r\n\r\n") === false) $res .= socket_read($socket, 2048);
 list($resp, $body) = explode("\r\n\r\n", $res, 2);
 $resp = explode("\r\n", $resp);
 $len  = 0;
+$h20x = strpos($resp[0], '200') || strpos($resp[0], '206');
 foreach($resp as $value) {
-  header($value);
   list($k, $v) = explode(':', $value, 2);
   if($k == 'Content-Length') $len = $v;
+  // Do not proxy other site's cookie.
+  if($k != 'Set-Cookie') header($value);
 }
 
 // Send content, end connection if finished.
 echo $body;
+
+if($h20x && $len == 0) {
+  // stream without size
+  while($out = socket_read($socket, 2048)) echo $out;
+  socket_close($socket);
+  exit;
+}
+
 $len -= strlen($body);
 while($len > 0) {
   $out = socket_read($socket, 2048);
